4122e13352b7aba7e8612b4e58c1d403cdd6cb89
Anush <np03cs4a230034@heraldcollege.edu.np>
2025-03-30 11:37:51 +0545

    Deleted All Files

--- CRUD/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UserProfileUpdateView, PasswordUpdateView
-
-urlpatterns = [
-    path('profile/update/', UserProfileUpdateView.as_view(), name='profile-update'),
-    path('password/update/', PasswordUpdateView.as_view(), name='password-update'),
-]
