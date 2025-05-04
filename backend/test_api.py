039a94a591bfd4a92098b317d9713a4e490f9cf5
Anush <np03cs4a230034@heraldcollege.edu.np>
2025-05-04 13:11:42 +0545

    changing the access code

--- backend/test_api.py
+++ backend/test_api.py
@@ -2,7 +2,7 @@ import requests
 
 url = "http://127.0.0.1:8000/crud/transactions/all/"
 headers = {
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQ2MzQwODk5LCJpYXQiOjE3NDYzMzk5OTksImp0aSI6ImNiMmE3ODgxZDY4ODQ3Yjg5ZDMwYWMzOTUxOWRiNWEzIiwidXNlcl9pZCI6NX0.dvaHMTr-hUXSWNAP8kJS8uAaN356oZeyAgDlzavWRMs"
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQ2MzQ0MDY4LCJpYXQiOjE3NDYzNDMxNjgsImp0aSI6IjY0NTM0NzcxOWM0ODQxZWJhNjI0MmY1ZTcwMjFiYzdkIiwidXNlcl9pZCI6NX0.1ep4tWN5zOmB10oEQI5aCoFikyIuNCwMra-YeroNLXk"
 }
 
 response = requests.get(url, headers=headers)
